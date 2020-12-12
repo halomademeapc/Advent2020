bce6dbdedc53b7c13f4b0cfe1cbcf48697281d3c
halomademeapc <alex@halomademeapc.com>
2020-12-12 16:27:59 -0500

    start day 10

---
+++ Advent2020.Tests/Day10Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Advent2020.Tests
+{
+    public class Day10Test
+    {
+        private int[] smallSample = new int[] { 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4 };
+
+        [Fact]
+        public void Small_Counts()
+        {
+            var jumps = Day10_PowerAdapters.GetJumps(smallSample);
+            Assert.Equal(7, jumps.Count(j => j == 1));
+            Assert.Equal(5, jumps.Count(j => j == 3));
+        }
+    }
+}
---
+++ Advent2020/Day10_PowerAdapters.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2020
+{
+    public class Day10_PowerAdapters : IPuzzleResult<long>
+    {
+        public long GetResult()
+        {
+            var outputs = Resources.Day10Values.Split(Environment.NewLine).Select(int.Parse);
+            var jumps = GetJumps(outputs);
+
+            var res = outputs.OrderBy(o => o).Zip(jumps).ToList();
+
+            return jumps.Count(j => j == 1) * jumps.Count(j => j == 3);
+        }
+
+        public static IEnumerable<int> GetJumps(IEnumerable<int> adapterOutputs)
+        {
+            var adapters = adapterOutputs.Select(o => new JoltageAdapter(o));
+            var device = GetDevice(adapters);
+            var outlet = new Outlet();
+
+            var allAdapters = adapters.Append(device).Append(outlet).OrderBy(a => a.Output);
+
+            return allAdapters.Select((adapter, index) => index > 0 ? adapter - allAdapters.ElementAt(index - 1) : 0);
+        }
+
+        public static Device GetDevice(IEnumerable<JoltageAdapter> adapters) => new Device(adapters.Max(a => a.Output));
+
+        public class JoltageAdapter
+        {
+            public JoltageAdapter(int output)
+            {
+                Output = output;
+            }
+
+            public int Output { get; private set; }
+
+            public virtual int InputCompensation { get; protected set; } = 3;
+
+            public virtual bool CanAcceptInputFrom(JoltageAdapter adapter) => adapter != this && Output - adapter.Output <= InputCompensation;
+
+            public static int operator -(JoltageAdapter adapter1, JoltageAdapter adapter2) => Math.Abs(adapter1.Output - adapter2.Output);
+        }
+
+        public class Device : JoltageAdapter
+        {
+            public Device(int highestAdapterJoltage) : base(highestAdapterJoltage + 3) { }
+        }
+
+        public class Outlet : JoltageAdapter
+        {
+            public Outlet() : base(0) { }
+
+            public override bool CanAcceptInputFrom(JoltageAdapter adapter) => false;
+        }
+
+
+    }
+    public class Day10_AdapterArrangements : IPuzzleResult<long>
+    {
+        public long GetResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
